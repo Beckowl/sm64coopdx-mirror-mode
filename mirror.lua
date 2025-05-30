a851dd5cd2d0334b67a9f4d35c1864b7c6f34010
pipocalio <bowserkoopah@gmail.com>
2025-05-30 01:19:24 -0300

    do the refactoring

--- mirror.lua
+++ mirror.lua
@@ -1,22 +1,18 @@
-local HOOK_RENDER_MARIO = 1
-local HOOK_RENDER_CAMERA = 2
-
-local G_CULL_BOTH = 0x00000200 | 0x00000400 -- G_CULL_BACK | G_CULL_FRONT
-local DEFAULT_GEOMETRY_MODE =
-    0x00000004 |                            -- G_SHADE
-    0x00200000 |                            -- G_SHADING_SMOOTH
-    0x00020000                              -- G_LIGHTING
+local G_CULL_BOTH = 0x00000200 | 0x00000400                         -- G_CULL_BACK | G_CULL_FRONT
+local DEFAULT_GEOMETRY_MODE = 0x00000004 | 0x00200000 | 0x00020000  -- G_SHADE | G_SHADING_SMOOTH | G_LIGHTING
 
 local G_RDPLOADSYNC = 0xe6
 local G_RDPPIPESYNC = 0xe7
 local G_RDPTILESYNC = 0xe8
 
-local CULLING_DISABLED = 2 -- This flag will be applied to the camera once culling is fully disabled
+local HOOK_RENDER_MARIO = 1
+local HOOK_PROCESS_CAMERA = 1
+
+local FLAG_CULLING_DISABLED = 16
+
+local cullingFixed = {}
 
 gMirrorEnabled = mod_storage_load_bool("mirrorEnabled") or not mod_storage_exists("mirrorEnabled")
-
-local camera
-local cullingDisabledModels = {}
 
 local function disable_face_culling(firstNode)
     local clear = G_CULL_BOTH
@@ -25,13 +21,13 @@ local function disable_face_culling(firstNode)
     geo_traverse_nodes(firstNode, function(node)
         local dl = graph_node_get_displaylist(node)
 
-        if not dl then
+        if not dl or cullingFixed[dl] then
             return
         end
 
         gfx_parse(dl, function(cmd, op)
             if op == G_GEOMETRYMODE then
-                clear = ~_SHIFTR(cmd.w0, 0, 24) | G_CULL_BOTH
+                clear = ~(cmd.w0 & ((1 << 24) - 1)) | G_CULL_BOTH
                 set = cmd.w1 & (~G_CULL_BOTH)
 
                 gfx_set_command(cmd, "gsSPGeometryMode(%i, %i)", clear, set)
@@ -42,99 +38,67 @@ local function disable_face_culling(firstNode)
                 gfx_set_command(cmd, "gsSPGeometryMode(%i, %i)", clear, set)
             end
         end)
+
+        cullingFixed[dl] = true
     end)
 end
 
-local function flip_camera(cameraNode)
-    cameraNode.matrixPtr.m00 = -cameraNode.matrixPtr.m00
-    cameraNode.matrixPtr.m10 = -cameraNode.matrixPtr.m10
-    cameraNode.matrixPtr.m20 = -cameraNode.matrixPtr.m20
-    cameraNode.matrixPtr.m30 = -cameraNode.matrixPtr.m30
+local function disable_object_face_culling(o, modelID)
+    local sharedChild = o.header.gfx.sharedChild
+    modelID = modelID or obj_get_model_id_extended(o)
 
-    cameraNode.matrixPtrPrev.m00 = -cameraNode.matrixPtrPrev.m00
-    cameraNode.matrixPtrPrev.m10 = -cameraNode.matrixPtrPrev.m10
-    cameraNode.matrixPtrPrev.m20 = -cameraNode.matrixPtrPrev.m20
-    cameraNode.matrixPtrPrev.m30 = -cameraNode.matrixPtrPrev.m30
-end
+    if not cullingFixed[modelID] and sharedChild then
+        disable_face_culling(sharedChild)
 
-local function on_mario_update(m)
-    if m.playerIndex ~= 0 then
-        return
+        cullingFixed[modelID] = true
     end
-
-    m.marioObj.hookRender = HOOK_RENDER_MARIO
 end
 
 local function on_object_render(o)
-    if o.hookRender == HOOK_RENDER_MARIO and not camera then
-        -- Three parents up from Mario's node is always the camera (GraphNodeCamera).
-        -- Some people call this the level node, but it's actually a camera.
-        -- The level display list nodes are children of this node.
-
-        camera = o.header.gfx.node.parent.parent.parent
-        camera.hookProcess = HOOK_RENDER_CAMERA
+    if o.hookRender == HOOK_RENDER_MARIO then
+        local camera = geo_get_current_camera()
+        camera.fnNode.node.hookProcess = HOOK_PROCESS_CAMERA
     end
 end
 
-local function on_warp(warpType)
-    if warpType == WARP_TYPE_CHANGE_AREA or warpType == WARP_TYPE_CHANGE_LEVEL or warpType == WARP_TYPE_NOT_WARPING then
-        camera = nil
+local function mario_update(m)
+    if m.playerIndex == 0 then
+        m.marioObj.hookRender = HOOK_RENDER_MARIO
     end
 end
 
-local function on_instant_warp()
-    camera = nil
-end
-
-local function on_object_set_model(o, modelID)
-    if not cullingDisabledModels[modelID] and o.header.gfx.sharedChild then
-        disable_face_culling(o.header.gfx.sharedChild)
-
-        cullingDisabledModels[modelID] = true
-    end
-end
-
-local function on_object_load(o)
-    if o == gMarioStates[0].marioObj then
-        camera = nil
-    end
-end
-
-local function on_geo_process(n)
-    if n.hookProcess ~= HOOK_RENDER_CAMERA then
+local function on_geo_process(node)
+    if node.type ~= GRAPH_NODE_TYPE_CAMERA then
         return
     end
 
-    if n.extraFlags & CULLING_DISABLED == 0 then
-        disable_face_culling(n)
+    if node.extraFlags & FLAG_CULLING_DISABLED == 0 then
+        disable_face_culling(node)
 
-        -- If i don't do this, some models (skeeters, boxes in WWW) turn invisible/inside out
-        -- isn't HOOK_OBJECT_SET_MODEL supposed to be called when an object spawns?
+        -- it seems some objects do not trigger HOOK_ON_OBJECT_SET_MODEL,
+        -- so i need to fix them here
         for o in iter_objects() do
-            local sharedChild = o.header.gfx.sharedChild
-
-            if sharedChild then
-                disable_face_culling(sharedChild)
-            end
+            disable_object_face_culling(o)
         end
 
-        n.extraFlags = n.extraFlags | CULLING_DISABLED
+        node.extraFlags = node.extraFlags | FLAG_CULLING_DISABLED
     end
 
-    local graphNodeCamera = cast_graph_node(n)
-
     if gMirrorEnabled then
-        flip_camera(graphNodeCamera)
+        local camera = cast_graph_node(node)
+
+        flip_matrix(camera.matrixPtr)
+        flip_matrix(camera.matrixPtrPrev)
     end
 end
 
 local function mirror_checkbox(_, value)
     gMirrorEnabled = value
 
-    if value == true then
+    if value then
         replace_directional_dialogs()
     else
-        smlua_text_utils_reset_all()
+        restore_directional_dialogs()
     end
 
     mod_storage_save_bool("mirrorEnabled", value)
@@ -143,9 +107,6 @@ end
 hook_mod_menu_checkbox("Mirroring enabled", gMirrorEnabled, mirror_checkbox)
 
 hook_event(HOOK_ON_OBJECT_RENDER, on_object_render)
+hook_event(HOOK_MARIO_UPDATE, mario_update)
 hook_event(HOOK_ON_GEO_PROCESS, on_geo_process)
-hook_event(HOOK_MARIO_UPDATE, on_mario_update)
-hook_event(HOOK_ON_INSTANT_WARP, on_instant_warp)
-hook_event(HOOK_ON_WARP, on_warp)
-hook_event(HOOK_OBJECT_SET_MODEL, on_object_set_model)
-hook_event(HOOK_ON_OBJECT_LOAD, on_object_load)
+hook_event(HOOK_OBJECT_SET_MODEL, disable_object_face_culling)
--- utils.lua
+++ utils.lua
@@ -28,18 +28,14 @@ function geo_traverse_nodes(firstNode, callback)
 end
 
 function graph_node_get_displaylist(node)
-    if node.type == GRAPH_NODE_TYPE_ANIMATED_PART or node.type == GRAPH_NODE_TYPE_DISPLAY_LIST or node.type == GRAPH_NODE_TYPE_TRANSLATION_ROTATION or node.type == GRAPH_NODE_TYPE_TRANSLATION or node.type == GRAPH_NODE_TYPE_ROTATION or node.type == GRAPH_NODE_TYPE_BILLBOARD then
-
+    if node.type == GRAPH_NODE_TYPE_ANIMATED_PART or node.type == GRAPH_NODE_TYPE_DISPLAY_LIST or node.type == GRAPH_NODE_TYPE_TRANSLATION_ROTATION or node.type == GRAPH_NODE_TYPE_TRANSLATION or node.type == GRAPH_NODE_TYPE_ROTATION or node.type == GRAPH_NODE_TYPE_BILLBOARD or node.type == GRAPH_NODE_TYPE_SCALE then
         return cast_graph_node(node).displayList
     end
 end
 
--- Implementations of the macros from gbi.h
-
-function _SHIFTL(v, s, w)
-    return ((v & ((1 << w) - 1)) << s) & 0xFFFFFFFF
-end
-
-function _SHIFTR(v, s, w)
-    return ((v >> s) & ((1 << w) - 1)) & 0xFFFFFFFF
+function flip_matrix(matrix)
+    matrix.m00 = -matrix.m00
+    matrix.m10 = -matrix.m10
+    matrix.m20 = -matrix.m20
+    matrix.m30 = -matrix.m30
 end
