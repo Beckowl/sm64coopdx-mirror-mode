551326f729dacb5040f33731d6e117025288555b
pipocalio <bowserkoopah@gmail.com>
2025-05-31 21:56:27 -0300

    Fix modelID oversight

--- mirror.lua
+++ mirror.lua
@@ -43,14 +43,17 @@ local function disable_face_culling(firstNode)
     end)
 end
 
-local function disable_object_face_culling(o, modelID)
+-- The modelID parameter of `HOOK_OBJECT_SET_MODEL` is NOT an extended model ID.
+-- I opened a PR to add an extended model ID as a third parameter.
+-- In hindsight, i'll add the param here so i won't need to update the code later.
+local function disable_object_face_culling(o, modelID, modelExtendedId)
     local sharedChild = o.header.gfx.sharedChild
-    modelID = modelID or obj_get_model_id_extended(o)
+    modelExtendedId = modelExtendedId or obj_get_model_id_extended(o)
 
-    if not cullingFixed[modelID] and sharedChild then
+    if not cullingFixed[modelExtendedId] and sharedChild then
         disable_face_culling(sharedChild)
 
-        cullingFixed[modelID] = true
+        cullingFixed[modelExtendedId] = true
     end
 end
 
@@ -75,8 +78,6 @@ local function on_geo_process(node)
     if node.extraFlags & FLAG_CULLING_DISABLED == 0 then
         disable_face_culling(node)
 
-        -- it seems some objects do not trigger HOOK_ON_OBJECT_SET_MODEL,
-        -- so i need to fix them here
         for o in iter_objects() do
             disable_object_face_culling(o)
         end
