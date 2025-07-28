b90f02b6b594ab69e0e294294b3b8b68a07c1dc5
pipocalio <bowserkoopah@gmail.com>
2025-07-28 03:55:22 -0300

    minor tweaks

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 -- name: \\#\\ Mirror Mode v2.0
 -- description: Mirror Mode\n\nA romhack compatible mod that flips the entire game world horizontally, so left becomes right and right becomes left.\n\nCamera controls are not flipped (unless you're using the vanilla camera), so you may want to toggle "Flip X axis" in your camera settings.
 -- category: gamemode
+-- pausable: false
 
 -- EMPTY `main.lua` GANG :fire: :fire:
